1a925a4030ad4722d57d4adbe96c2a36e19af21c
milosbujakovic <milosbujakovic97@gmail.com>
2018-07-11 22:21:24 +0200

    Dodato logout dugme u MainWindow.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,24 +1,56 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import main.Main;
 import model.Korisnik;
+import utility.Utility;
 
 
 public class MainWindow extends JFrame {
 	public MainWindow(String title, Korisnik k) {
 		//TODO: Implement
 		super(title);
+		 
 		this.setVisible(true);
 
 		this.setSize(500,200);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		
+		setLocationRelativeTo(null); // vazno da se poziva nakon setSize()
+		
+		JButton btnLogout = new JButton("Logout");
+		JPanel jp = new JPanel();
+        jp.add(btnLogout);
+        getContentPane().add(jp, BorderLayout.EAST);
+		
+        btnLogout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	dispose();
+            	Main.start();
+            	System.exit(0);
+            }
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 		switch(k.getVrsta()) {
 		case Operater:
 			this.prikaziMenuOperatera();
