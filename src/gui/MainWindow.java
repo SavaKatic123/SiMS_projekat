dce9e457d28d489e09a06d665a37d1248da0a22e
MilosBujakovic <milosbujakovic97@gmail.com>
2018-07-13 18:16:03 +0200

    Doradjen GUI
    Poradjeno na meniju za operatera, uz obracanje paznje na to da polje za
    unos bude krupno.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,14 +1,22 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 import main.Main;
 import model.Korisnik;
@@ -26,7 +34,7 @@ public class MainWindow extends JFrame {
 		 
 		this.setVisible(true);
 
-		this.setSize(500,200);
+		this.setSize(1000,700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
@@ -62,9 +70,19 @@ public class MainWindow extends JFrame {
 	}
 	public void prikaziMenuOperatera() {
 		
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(new GridLayout(5, 5));
 		this.add(panel);
+		//getContentPane().add(panel, BorderLayout.WEST);
+		GridBagConstraints cs = new GridBagConstraints();
+        
+        cs.fill = GridBagConstraints.HORIZONTAL;
+        
 		JButton button = new JButton("Pokreni rampu");
+		button.setFont(new Font("Serif", Font.BOLD, 20));
+		cs.gridx = 0;
+        cs.gridy = 0;
+        cs.gridwidth = 1;
+        
 		panel.add(button);
 		
 		button.addActionListener (new ActionListener() {
@@ -88,9 +106,24 @@ public class MainWindow extends JFrame {
 				r.setStanje(r.getStanje().getSledeceStanje());
 				
 			}
-			
 		});
 		
+		//panel.setBorder(new LineBorder(Color.GRAY));
+		JLabel labela = new JLabel("Broj registarske tablice: ");
+		labela.setFont(new Font("Serif", Font.BOLD, 20));
+		
+		cs.gridx = 0;
+        cs.gridy = 1;
+        cs.gridwidth = 1;
+        
+		panel.add(labela);
+		
+		JTextField unosPolje = new JTextField(20);
+		cs.gridx = 1;
+        cs.gridy = 1;
+        cs.gridwidth = 2;	
+		
+		panel.add(unosPolje);
 	}
 	
 	public void prikaziMenuAdministratora() {
